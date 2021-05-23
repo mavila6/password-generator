c9bc37b5a7a5bfcc89aed359d58f2294ae8126f9
Marco Avila <mavila6@email.arizona.com>
2021-05-23 12:54:10 -0700

    split strings into arrays, added variables to be used later, added if statements to ensure a number is entered for password length

--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,6 @@ var lowercase = 'abcdefghijklmnopqrstuvwxyz';
 var numbers = '0123456789';
 var specials = '!@#$%^&*()_+=<>/?~';
 
-
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -17,6 +14,33 @@ function writePassword() {
 
     passwordText.value = password;
 }
+
+var Capitals = uppercase.split('');
+var Lowers = lowercase.split('');
+var Nums = numbers.split('');
+var specs =specials.split('');
+
+function generatePassword(){
+
+var characters = [];
+var pwd = '';
+
+var pwlength = prompt('Insert a password length between 8 and 128 characters');
+
+if (pwlength < 8 || pwlength > 128) {
+    alert('Password length must be between 8 and 128 characters. Please refresh the page and try again.');
+    return;
+};
+
+if (isNaN(pwlength) === true) {
+    alert('Must enter a number for password length. Please refresh the page and try again.');
+    return;
+};
+
+
+
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
