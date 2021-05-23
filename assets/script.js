0cdb95fb8d5219d14deb4f9d7d5f64b10beb9ba7
Marco Avila <mavila6@email.arizona.com>
2021-05-23 13:09:18 -0700

    added if statements for confirm variables

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,7 @@ function writePassword() {
 var Capitals = uppercase.split('');
 var Lowers = lowercase.split('');
 var Nums = numbers.split('');
-var specs =specials.split('');
+var Specs =specials.split('');
 
 function generatePassword(){
 
@@ -30,12 +30,33 @@ var pwlength = prompt('Insert a password length between 8 and 128 characters');
 if (pwlength < 8 || pwlength > 128) {
     alert('Password length must be between 8 and 128 characters. Please refresh the page and try again.');
     return;
-};
+}
 
 if (isNaN(pwlength) === true) {
     alert('Must enter a number for password length. Please refresh the page and try again.');
     return;
-};
+}
+
+var upperChar = confirm('Would you like to include capital letters?');
+var lowerChar = confirm('Would you like to include lowercase letters?');
+var numChar = confirm('Would you like to include numbers');
+var specialChar = confirm('Would you like to include special characters?');
+
+if (upperChar === true) {
+    characters = characters.concat(Capitals);
+}
+
+if (lowerChar === true) {
+    characters = characters.concat(Lowers);
+}
+
+if (numChar === true) {
+    characters = characters.concat(Nums);
+}
+
+if (specialChar === true) {
+    characters = characters.concat(Specs);
+}
 
 
 
